af0accde7062720348047997bcd5a0eea634089a
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-08 11:26:04 +0100

    FW.Sprite now has an X and Y position.

--- FW/Sprite.lua
+++ FW/Sprite.lua
@@ -2,11 +2,12 @@ local class = require 'libs/middleclass'
 
 local Sprite = class('Sprite')
 
-function Sprite:initialize()
+function Sprite:initialize(x,y)
+    self.x = x
+    self.y = y
     self.image = love.graphics.newImage('assets/green-tiles.png')
     self.scale = 3
 end
-
 
 return Sprite
 
