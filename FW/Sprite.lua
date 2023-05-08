6264295935343d09fa765e2ce0a7ba46ef9bf302
lukehayes <lukepurplehayes@gmail.com>
2023-05-08 18:40:46 +0100

    Added starting implementation of animation frames

--- FW/Sprite.lua
+++ FW/Sprite.lua
@@ -2,26 +2,29 @@ local class = require 'libs/middleclass'
 
 local Sprite = class('Sprite')
 
-function Sprite:initialize(x,y, sprite_path)
+function Sprite:initialize(x,y, sprite_path, quad_x, quad_y)
     self.x = x
     self.y = y
-    self.scale = 3
     self.image = love.graphics.newImage(sprite_path)
     self.image:setFilter('nearest', 'nearest')
+    self.scale = 10
     self.tile_size = 16
+    self.quad_x = quad_x
+    self.quad_y = quad_y
+
 
     --Animation config
     self.frame_start = 1
-    self.frame_end = 4
-    self.anim_speed = 0.1
+    self.frame_end = 6
+    self.anim_speed = 0.5
     self.anim_timer = 0
-    self.current_tile = 0
+    self.current_tile = 1
     --self.quad = love.graphics.newQuad()
 end
 
 
 function Sprite:draw()
-    local quad = love.graphics.newQuad(self.tile_size * self.current_tile,0,self.tile_size, self.tile_size, self.image)
+    local quad = love.graphics.newQuad(self.current_tile,self.quad_y * self.tile_size, self.tile_size, self.tile_size, self.image)
     love.graphics.draw(self.image,quad, self.x, self.y,0, self.scale, self.scale)
 end
 
@@ -30,13 +33,12 @@ function Sprite:update(dt)
     self.anim_timer = self.anim_timer + dt
 
     if(self.anim_timer >= self.anim_speed) then
-        self.current_tile = self.current_tile + 1
+        self.quad_x = self.quad_x + 1
+        self.current_tile = self.quad_x * self.tile_size
         self.anim_timer = 0
     end
 
-    if(self.current_tile == self.frame_end) then
-        self.current_tile = 0
-    end
+    print(self.current_tile)
 end
 
 return Sprite
--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 local Sprite = require('FW.Sprite')
 local Renderer = require('FW.Renderer')
 
-local so = Sprite:new(100,400)
+local so = Sprite:new(100,400, 'assets/flames.png',0,0)
+local s2 = Sprite:new(300,100, 'assets/tiles.png',43,10)
 local rr = Renderer:new()
 
 local c = 0
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 end
 
 function love.update(dt)
@@ -16,12 +18,18 @@ function love.update(dt)
     c = c + 0.05
 
     so:update(dt)
+    s2:update(dt)
 end
 
 function love.draw()
     --love.graphics.rectangle("fill",100,100,100,100)
     rr:drawSprite(so)
     so:draw();
+    s2:draw();
 end
 
-
+function love.keypressed(key)
+   if key == "escape" then
+    s2:update(_G.dt)
+   end
+end
