059246d9b90e22ce75cd6cab4ba3e097e943c8bb
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-08 11:16:29 +0100

    Implemented basic Sprite class

--- FW/Sprite.lua
+++ FW/Sprite.lua
@@ -1,1 +1,12 @@
 local class = require 'libs/middleclass'
+
+local Sprite = class('Sprite')
+
+function Sprite:initialize()
+    self.image = love.graphics.newImage('assets/green-tiles.png')
+    self.scale = 3
+end
+
+
+return Sprite
+
