e9c692e6ce229ddb026e628a143dc84301964813
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-09 11:25:49 +0100

    Added test container to main.lu

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 local Sprite = require('FW.Sprite')
+local AnimSprite = require('FW.AnimatedSprite')
 local Renderer = require('FW.Renderer')
 
-local so = Sprite:new(100,400, 'assets/flames.png',0,0)
-local s2 = Sprite:new(300,100, 'assets/tiles.png',43,10)
+local so = Sprite:new(300,100, 'assets/tiles.png',5,8)
+local s2 = AnimSprite:new(100,400, 'assets/tiles.png',12, 21,26)
 local rr = Renderer:new()
 
 local c = 0
+
+local container = {}
+table.insert(container, so)
+table.insert(container, s2)
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -15,21 +20,21 @@ function love.update(dt)
     -- Hacky but it works - make delta time global.
     _G.dt = dt
 
-    c = c + 0.05
-
-    so:update(dt)
-    s2:update(dt)
+    for k,v in ipairs(container) do
+       v:update(dt)
+    end
 end
 
 function love.draw()
     --love.graphics.rectangle("fill",100,100,100,100)
     rr:drawSprite(so)
-    so:draw();
-    s2:draw();
+    for k,v in ipairs(container) do
+       v:draw()
+    end
 end
 
 function love.keypressed(key)
    if key == "escape" then
-    s2:update(_G.dt)
+    so:update(_G.dt)
    end
 end
