80e6c65c2a19e03615afde5c9c850f68a4307f98
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-09 11:03:18 +0100

    Sprites can now be animated

--- FW/Sprite.lua
+++ FW/Sprite.lua
@@ -2,43 +2,46 @@ local class = require 'libs/middleclass'
 
 local Sprite = class('Sprite')
 
-function Sprite:initialize(x,y, sprite_path, quad_x, quad_y)
+function Sprite:initialize(x,y, sprite_path, quad_y, frame_start, frame_end)
     self.x = x
     self.y = y
     self.image = love.graphics.newImage(sprite_path)
     self.image:setFilter('nearest', 'nearest')
     self.scale = 10
     self.tile_size = 16
-    self.quad_x = quad_x
     self.quad_y = quad_y
 
 
     --Animation config
-    self.frame_start = 1
-    self.frame_end = 6
-    self.anim_speed = 0.5
+    self.frame_start = frame_start
+    self.frame_end = frame_end
+    self.anim_speed = 0.1
     self.anim_timer = 0
-    self.current_tile = 1
-    --self.quad = love.graphics.newQuad()
+    self.current_frame = self.frame_start
 end
 
 
 function Sprite:draw()
-    local quad = love.graphics.newQuad(self.current_tile,self.quad_y * self.tile_size, self.tile_size, self.tile_size, self.image)
+    local quad = love.graphics.newQuad(self.current_frame * self.tile_size,self.quad_y * self.tile_size, self.tile_size, self.tile_size, self.image)
     love.graphics.draw(self.image,quad, self.x, self.y,0, self.scale, self.scale)
+end
+
+function Sprite:animate()
+    self.anim_timer = self.anim_timer + dt
+
+    if(self.anim_timer >= self.anim_speed) then
+        self.anim_timer = 0
+        self.current_frame = self.current_frame + 1
+    end
+
+    if(self.current_frame == self.frame_end) then
+        self.current_frame = self.frame_start
+    end
 end
 
 
 function Sprite:update(dt)
-    self.anim_timer = self.anim_timer + dt
-
-    if(self.anim_timer >= self.anim_speed) then
-        self.quad_x = self.quad_x + 1
-        self.current_tile = self.quad_x * self.tile_size
-        self.anim_timer = 0
-    end
-
-    print(self.current_tile)
+    self:animate()
 end
 
 return Sprite
