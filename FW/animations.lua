7ffd6f4aaf1d477dfdb8589aad9308a8d3547798
lukehayes <lukepurplehayes@gmail.com>
2023-05-12 17:09:43 +0100

    Added 'HatChar' animation frames table

--- FW/animations.lua
+++ FW/animations.lua
@@ -59,31 +59,57 @@ local animations = {
     },
 
    -------------------------------------------------
-   -- GHOST
+   -- HatChar
    -------------------------------------------------
-    ['ghost'] = {
-        ['walk'] = {
+    ['hatchar'] = {
+        ['idle'] = {
             ['down'] = {
-                frame_x = 20,
-                frame_y = 8,
+                frame_x = 18,
+                frame_y = 0,
                 frame_num = 4
             },
 
             ['up'] = {
-                frame_x = 20,
-                frame_y = 9,
+                frame_x = 18,
+                frame_y = 1,
                 frame_num = 4
             },
 
             ['right'] = {
-                frame_x = 20,
-                frame_y = 10,
+                frame_x = 18,
+                frame_y = 2,
                 frame_num = 4
             },
 
             ['left'] = {
-                frame_x = 20,
-                frame_y = 11,
+                frame_x = 18,
+                frame_y = 3,
+                frame_num = 4
+            },
+        },
+
+        ['walk'] = {
+            ['down'] = {
+                frame_x = 18,
+                frame_y = 4,
+                frame_num = 4
+            },
+
+            ['up'] = {
+                frame_x = 18,
+                frame_y = 5,
+                frame_num = 4
+            },
+
+            ['right'] = {
+                frame_x = 18,
+                frame_y = 6,
+                frame_num = 4
+            },
+
+            ['left'] = {
+                frame_x = 18,
+                frame_y = 7,
                 frame_num = 4
             },
         }
