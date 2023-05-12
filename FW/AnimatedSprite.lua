c969e64b68f0f9c2d6f1aa65756d2b1d5510cda2
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-12 10:11:31 +0100

    Moved animation data to FW.animations.lua

--- FW/AnimatedSprite.lua
+++ FW/AnimatedSprite.lua
@@ -29,61 +29,7 @@ function AnimatedSprite:initialize(x,y, path)
     self.anim_speed = 0.15
     self.anim_direction = 'down'
 
-    -- TODO This works for now but abstracting 
-    -- this in way would be good.
-    self.animations = {
-        ['idle'] = {
-            ['down']= {
-                frame_x = 13,
-                frame_y = 8,
-                frame_num = 4
-            },
-
-            ['up'] = {
-                frame_x = 13,
-                frame_y = 9,
-                frame_num = 4
-            },
-
-            ['right'] = {
-                frame_x = 13,
-                frame_y = 10,
-                frame_num = 4
-            },
-
-            ['left'] = {
-                frame_x = 13,
-                frame_y = 11,
-                frame_num = 4
-            }
-        },
-
-        ['walk'] = {
-            ['down']= {
-                frame_x = 13,
-                frame_y = 0,
-                frame_num = 4
-            },
-
-            ['up'] = {
-                frame_x = 13,
-                frame_y = 1,
-                frame_num = 4
-            },
-
-            ['right'] = {
-                frame_x = 13,
-                frame_y = 2,
-                frame_num = 4
-            },
-
-            ['left'] = {
-                frame_x = 13,
-                frame_y = 3,
-                frame_num = 4
-            }
-        }
-    }
+    self.animations = require 'FW.animations'
 
     self.current_anim = self.animations['idle']['down']
     self.current_frame = self.current_anim.frame_x
---
+++ FW/animations.lua
@@ -0,0 +1,57 @@
+
+local animations = {
+
+        ['idle'] = {
+            ['down']= {
+                frame_x = 13,
+                frame_y = 8,
+                frame_num = 4
+            },
+
+            ['up'] = {
+                frame_x = 13,
+                frame_y = 9,
+                frame_num = 4
+            },
+
+            ['right'] = {
+                frame_x = 13,
+                frame_y = 10,
+                frame_num = 4
+            },
+
+            ['left'] = {
+                frame_x = 13,
+                frame_y = 11,
+                frame_num = 4
+            }
+        },
+
+        ['walk'] = {
+            ['down']= {
+                frame_x = 13,
+                frame_y = 0,
+                frame_num = 4
+            },
+
+            ['up'] = {
+                frame_x = 13,
+                frame_y = 1,
+                frame_num = 4
+            },
+
+            ['right'] = {
+                frame_x = 13,
+                frame_y = 2,
+                frame_num = 4
+            },
+
+            ['left'] = {
+                frame_x = 13,
+                frame_y = 3,
+                frame_num = 4
+            }
+        }
+    }
+
+return animations
