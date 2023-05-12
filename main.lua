2e7918f71e6647f44bdaa955b47e727bf4dce7e2
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-12 10:41:11 +0100

    Added animation test code to main

--- main.lua
+++ main.lua
@@ -5,20 +5,17 @@ local Container = require('FW.Container')
 local Object = require('FW.Object')
 local EventManager = require('FW.EventManager')
 
-local so = Sprite:new(0,0, 'assets/trees.png',2,0)
-so.tile_size = 256
-so.scale = 0.5
-local s2 = AnimSprite:new(100,400, 'assets/tiles.png',12, 12, 6)
-local s3 = AnimSprite:new(400,400, 'assets/tiles.png')
+local s3 = AnimSprite:new(350,250, 'assets/tiles.png')
 local c = Container:new()
-local c2 = Container:new()
+c:add(s3)
 
-c:add(so)
 local em = EventManager:new()
 
+local animChar = 'ghost'
+local animMode = 'walk'
 
-function fillContainer(c)
-    for n=1,c do
+function fillContainer(num)
+    for n=1,num do
         local w = love.graphics.getWidth()
         local h = love.graphics.getHeight()
         local rx = love.math.random(10,w)
@@ -27,7 +24,7 @@ function fillContainer(c)
         local o = Sprite:new(rx,ry, 'assets/trees.png', love.math.random(0,4),0)
         o.scale = 0.5
         o.tile_size = 256
-        c2:add(o)
+        c:add(o)
     end
 end
 
@@ -41,6 +38,7 @@ function love.update(dt)
     _G.dt = dt
 
     em:run('update_begin')
+
     c:update(dt)
 
     local speed = 200
@@ -48,22 +46,22 @@ function love.update(dt)
     -- TODO Refactor this out into the player class.
     if love.keyboard.isDown('w') then
         s3.y = s3.y - 1 * speed * dt
-        s3.current_anim = s3.animations['walk']['up']
+        s3.current_anim = s3.animations[animChar][animMode]['up']
         s3.anim_direction = 'up'
     elseif love.keyboard.isDown('s') then
         s3.y = s3.y + 1 * speed * dt
-        s3.current_anim = s3.animations['walk']['down']
+        s3.current_anim = s3.animations[animChar][animMode]['down']
         s3.anim_direction = 'down'
     elseif love.keyboard.isDown('a') then
         s3.x = s3.x - 1 * speed * dt
-        s3.current_anim = s3.animations['walk']['left']
+        s3.current_anim = s3.animations[animChar][animMode]['left']
         s3.anim_direction = 'left'
     elseif love.keyboard.isDown('d') then
         s3.x = s3.x + 1 * speed * dt
         s3.anim_direction = 'right'
-        s3.current_anim = s3.animations['walk']['right']
+        s3.current_anim = s3.animations[animChar][animMode]['right']
     else 
-        s3.current_anim = s3.animations['idle'][s3.anim_direction]
+        s3.current_anim = s3.animations['knight']['idle'][s3.anim_direction]
     end
     em:run('update_end')
 end
@@ -73,16 +71,14 @@ function love.draw()
     --love.graphics.rectangle("fill",100,100,100,100)
    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 
-    love.graphics.setBackgroundColor(0.5,0.7,0.5,1.0)
+    love.graphics.setBackgroundColor(0.7,0.7,0.7,1.0)
     c:draw()
-    c2:draw()
-    s3:draw()
     em:run('draw_end')
 end
 
 function love.keypressed(key)
    if key == "space" then
-       c2 = Container:new()
+       c = Container:new()
        print(love.graphics.getWidth())
        fillContainer(love.graphics.getWidth() / 10)
    end
