1b0663a759a50715deef4aa0a5e1b03aac8db481
lukehayes <lukepurplehayes@gmail.com>
2023-05-15 16:45:35 +0100

    Tidied up old test code in main.lua

--- main.lua
+++ main.lua
@@ -1,34 +1,13 @@
 local Sprite = require('FW.Sprite')
 local AnimSprite = require('FW.AnimatedSprite')
-local Renderer = require('FW.Renderer')
 local Container = require('FW.Container')
-local Object = require('FW.Object')
 local EventManager = require('FW.EventManager')
 
-local s3 = AnimSprite:new(350,250, 'assets/tiles.png')
+local player = AnimSprite:new(350,250, 'assets/tiles.png')
 local c = Container:new()
-local trees = Container:new()
-c:add(s3)
+c:add(player)
 
 local em = EventManager:new()
-
-local animChar = 'knight'
-local animMode = 'idle'
-
-function fillContainer(num)
-    for n=1,num do
-        local w = love.graphics.getWidth()
-        local h = love.graphics.getHeight()
-        local rx = love.math.random(10,w)
-        local ry = love.math.random(10,h)
-        local speed = love.math.random(10,100)
-        local o = Sprite:new(rx,ry, 'assets/trees.png', love.math.random(0,4),0)
-        o.scale = 0.3
-        o.tile_size = 256
-        trees:add(o)
-    end
-end
-
 
 function love.load()
     em:emit('init')
@@ -41,43 +20,46 @@ function love.update(dt)
     em:emit('update_begin')
 
     c:update(dt)
-    trees:update(dt)
+    playerInput()
 
-    local speed = 200
-
-    -- TODO Refactor this out into the player class.
-    if love.keyboard.isDown('w') then
-        s3.y = s3.y - 1 * speed * dt
-        s3:play(animChar, 'walk', 'up')
-    elseif love.keyboard.isDown('s') then
-        s3.y = s3.y + 1 * speed * dt
-        s3:play(animChar, 'walk', 'down')
-    elseif love.keyboard.isDown('a') then
-        s3.x = s3.x - 1 * speed * dt
-        s3:play(animChar, 'walk', 'left')
-    elseif love.keyboard.isDown('d') then
-        s3.x = s3.x + 1 * speed * dt
-        s3:play(animChar, 'walk', 'right')
-    else 
-        s3:play(animChar, 'idle', s3.anim_direction)
-    end
     em:emit('update_end')
 end
 
 function love.draw()
     em:emit('draw_begin')
     --love.graphics.rectangle("fill",100,100,100,100)
-   love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+   love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 100, 10,0, 2,2)
 
     love.graphics.setBackgroundColor(0.7,0.7,0.7,1.0)
     c:draw()
-    trees:draw()
     em:emit('draw_end')
 end
 
 function love.keypressed(key)
    if key == "space" then
-       trees = Container:new()
-       fillContainer(love.math.random(10,100))
+       print('Space Key Pressed')
    end
+end
+
+
+function playerInput()
+    -- TODO Refactor this out into the player class.
+    
+    local speed = 200
+
+    if love.keyboard.isDown('w') then
+        player.y = player.y - 1 * speed * dt
+        player:play('knight', 'walk', 'up')
+    elseif love.keyboard.isDown('s') then
+        player.y = player.y + 1 * speed * dt
+        player:play('knight', 'walk', 'down')
+    elseif love.keyboard.isDown('a') then
+        player.x = player.x - 1 * speed * dt
+        player:play('knight', 'walk', 'left')
+    elseif love.keyboard.isDown('d') then
+        player.x = player.x + 1 * speed * dt
+        player:play('knight', 'walk', 'right')
+    else 
+        player:play('knight', 'idle', player.anim_direction)
+    end
 end
