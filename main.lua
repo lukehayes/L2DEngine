dda418c51142ebaa2c015a577992ae5ceb3c16d5
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-16 11:04:46 +0100

    Tidied up main a little bit

--- main.lua
+++ main.lua
@@ -6,17 +6,18 @@ local EventManager = require('FW.EventManager')
 
 local player = AnimSprite:new(350,250, 'assets/tiles.png')
 local star   = AnimSpriteRow:new(300,250, 'assets/pixel-star.png', 0,3)
-local c = Container:new()
-c:add(player)
 
 local em = EventManager:new()
 em:emit('init')
 
-
 function love.load()
     em:emit('engine_load')
     _G.game = {} -- Location for all helpful global functions, values etc.
+    _G.game.default_container = Container:new()
+    _G.game.default_container:add(player)
+    _G.game.default_container:add(star)
 end
+
 
 function love.update(dt)
     -- Hacky but it works - make delta time global.
@@ -24,9 +25,7 @@ function love.update(dt)
 
     em:emit('update_begin')
 
-    star:update(dt)
-
-    c:update(dt)
+    _G.game.default_container:update(dt)
     playerInput()
 
     em:emit('update_end')
@@ -35,23 +34,17 @@ end
 function love.draw()
     em:emit('draw_begin')
     --love.graphics.rectangle("fill",100,100,100,100)
-   love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 100, 10,0, 2,2)
+    love.graphics.setBackgroundColor(0.2,0.6,0.2,1)
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 100, 10,0, 2,2)
+
+    _G.game.default_container:draw()
 
     em:emit('draw_end')
-
-    star:draw()
 end
-
-function love.keypressed(key)
-   if key == "space" then
-       print('Space Key Pressed')
-   end
-end
-
 
 function playerInput()
     -- TODO Refactor this out into the player class.
-    
+
     local speed = 200
 
     if love.keyboard.isDown('w') then
