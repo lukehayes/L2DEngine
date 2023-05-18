2a9cb54b6a98c9c91a338080422dc65d8b349450
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-18 09:22:10 +0100

    Moved Sprite quad generation to constructor.

--- FW/Sprite.lua
+++ FW/Sprite.lua
@@ -27,6 +27,7 @@ function Sprite:initialize(x,y, path, frame_x, frame_y)
     self.tile_size = 16
     self.frame_x = frame_x
     self.frame_y = frame_y
+    self.quad = love.graphics.newQuad(self.frame_x * self.tile_size,self.frame_y * self.tile_size, self.tile_size, self.tile_size, self.image)
 end
 
 
@@ -35,8 +36,7 @@ end
 --
 -- @return nil
 function Sprite:draw()
-    local quad = love.graphics.newQuad(self.frame_x * self.tile_size,self.frame_y * self.tile_size, self.tile_size, self.tile_size, self.image)
-    love.graphics.draw(self.image,quad, self.x, self.y,0, self.scale, self.scale)
+    love.graphics.draw(self.image,self.quad, self.x, self.y,0, self.scale, self.scale)
 end
 
 --------------------------------------------------------------------------------
