9154ff172615aceacb99384807b7ad3b48ed294c
lukehayes <lukepurplehayes@gmail.com>
2023-05-19 17:45:00 +0100

    Removed old test code from main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ local EventManager  = require('FW.EventManager')
 
 local em        = EventManager:new()
 em:emit('init')
-local playerObj = AnimSprite:new(500,320, 'assets/tiles.png', 5,10, 10)
 
 function love.load()
     em:emit('engine_load')
@@ -17,11 +16,8 @@ function love.load()
 
     -- Add objects to the container.
     -- 
-    bg = RawImage:new(0,0, 192,144, 'assets/bg.png', 5)
+    local bg = RawImage:new(0,0, 192,144, 'assets/bg.png', 5)
     _G.game.default_container:add(bg)
-
-    _G.game.default_container:add(playerObj)
-
 end
 
 
@@ -33,33 +29,13 @@ function love.update(dt)
 
     _G.game.default_container:update(dt)
 
-    if love.keyboard.isDown('w') then
-        playerObj.direction = 'up'
-        playerObj.state = 'walk'
-        playerObj.y = playerObj.y - 100 * dt
-    elseif love.keyboard.isDown('s') then
-        playerObj.direction = 'down'
-        playerObj.state = 'walk'
-        playerObj.y = playerObj.y + 100 * dt
-    elseif love.keyboard.isDown('a') then
-        playerObj.direction = 'left'
-        playerObj.state = 'walk'
-        playerObj.x = playerObj.x - 100 * dt
-    elseif love.keyboard.isDown('d') then
-        playerObj.direction = 'right'
-        playerObj.state = 'walk'
-        playerObj.x = playerObj.x + 100 * dt
-    else 
-        playerObj.state = 'idle'
-    end
-
     em:emit('update_end')
 end
 
 function love.draw()
     em:emit('draw_begin')
     --love.graphics.rectangle("fill",100,100,100,100)
-    love.graphics.setBackgroundColor(0.2,0.6,0.2,1)
+    love.graphics.setBackgroundColor(0.2,0.7,0.4,1)
     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), _G.game.default_font,10, 10,0, 4,4)
 
     _G.game.default_container:draw()
