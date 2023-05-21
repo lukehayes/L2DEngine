95a4a1af4bb74e7a3ca48494a4c1b1eb8c946b30
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-21 11:24:14 +0100

    Added ObjectFactory:generateEntities()

--- FW/Factory/ObjectFactory.lua
+++ FW/Factory/ObjectFactory.lua
@@ -5,6 +5,7 @@
 local class  = require 'libs/middleclass'
 local Color  = require 'FW.GFX.Color'
 local Object = require('FW.Core.Object')
+local Entity  = require('FW.Entity.Entity')
 
 local ObjectFactory = class('ObjectFactory')
 
@@ -34,6 +35,25 @@ function ObjectFactory.static:generate(count)
         end
     end
 end
+
+--------------------------------------------------------------------------------
+-- Generate N number of ENTITIES for testing.
+--
+-- @tparam number count The number of objects to draw.
+--
+-- @return nil.
+function ObjectFactory.static:generateEntities(count)
+    for x = 1,count do
+        for y = 1,count do
+            local rx = love.math.random(10,600)
+            local ry = love.math.random(10,600)
+            local o  = Entity:new(rx,ry, 4)
+            o.color  = Color:random()
+            _G.game.default_container:add(o)
+        end
+    end
+end
+
 
 return ObjectFactory
 
