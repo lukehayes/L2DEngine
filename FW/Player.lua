ef1c1c1b01dccce942503ce208513dae5054e2af
lukehayes <lukepurplehayes@gmail.com>
2023-05-22 18:09:00 +0100

    FW.Player class is now usable for VERY simple tests.

--- FW/Player.lua
+++ FW/Player.lua
@@ -1,34 +1,40 @@
 --------------------------------------------------------------------------------
--- General player class that inherits from AnimatedSprite
+-- General player class for testing that only inherits from FW.Core.Object
+-- and has simple player movement control.
 --
 -- @classmod Player
-local class = require 'libs/middleclass'
-local AnimatedSprite = require 'FW.AnimatedSprite'
+local class  = require 'libs/middleclass'
+local Object = require 'FW.Core.Object'
+local Color  = require 'FW.GFX.Color'
 
-local Player = class('Player', AnimatedSprite)
+local Player = class('Player', Object)
 
 --------------------------------------------------------------------------------
 -- Constructor.
 --
 -- @tparam number x The x position of the sprite.
 -- @tparam number y The y position of the sprite.
--- @tparam string path The path to an image of tiles.
---
--- @tparam number anim_self.speed How fast the animation will run at.
--- @tparam string anim_direction The direction of the animation (up,down,left,right).
--- @tparam string current_anim The current animation the sprite is using.
--- @tparam number current_frame The current frame the animation is on.
 --
 -- @usage
 -- -- local AnimatiedSprite = require 'FW.AnimatiedSprite'
 -- -- local AnimSpr = AnimatedSprite:new(100,100, 'tiles.png')
-function Player:initialize(x,y, path)
+function Player:initialize(x,y)
+    Object.initialize(self, x,y)
 
-    AnimatedSprite.initialize(self, x,y,path)
+    self.color = Color:new(0,1,1,1)
+    self.w = 50
+    self.h = 50
+    self.speed = 250
+end
 
-    self.animations = require 'FW.animations'
-    self.current_anim = self.animations['knight'][self.state][self.direction]
-    self.speed = 200
+function Player:draw()
+    love.graphics.setColor(
+        self.color.r,
+        self.color.g,
+        self.color.b,
+        self.color.a
+    )
+    love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
 end
 
 function Player:update(dt)
@@ -37,40 +43,15 @@ end
 
 function Player:playerInput(dt)
 
-    --if love.keyboard.isDown('w') then
-        --playerObj.direction = 'up'
-        --playerObj.state = 'walk'
-        --playerObj.y = playerObj.y - 100 * dt
-    --elseif love.keyboard.isDown('s') then
-        --playerObj.direction = 'down'
-        --playerObj.state = 'walk'
-        --playerObj.y = playerObj.y + 100 * dt
-    --elseif love.keyboard.isDown('a') then
-        --playerObj.direction = 'left'
-        --playerObj.state = 'walk'
-        --playerObj.x = playerObj.x - 100 * dt
-    --elseif love.keyboard.isDown('d') then
-        --playerObj.direction = 'right'
-        --playerObj.state = 'walk'
-        --playerObj.x = playerObj.x + 100 * dt
-    --else 
-        --playerObj.state = 'idle'
-    --end
-
     if love.keyboard.isDown('w') then
         self.y = self.y - 1 * self.speed * dt
-        self:play('knight', 'walk', 'up')
     elseif love.keyboard.isDown('s') then
         self.y = self.y + 1 * self.speed * dt
-        self:play('knight', 'walk', 'down')
     elseif love.keyboard.isDown('a') then
         self.x = self.x - 1 * self.speed * dt
-        self:play('knight', 'walk', 'left')
     elseif love.keyboard.isDown('d') then
         self.x = self.x + 1 * self.speed * dt
-        self:play('knight', 'walk', 'right')
     else 
-        self:play('knight', 'idle', self.anim_direction)
     end
 end
 
