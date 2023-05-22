4a32588b1207f4b25e620b888fac1124c5c33e91
lukehayes <lukepurplehayes@gmail.com>
2023-05-22 18:04:43 +0100

    Added Game.width() and Game.height() functions.

--- FW/Core/Game.lua
+++ FW/Core/Game.lua
@@ -14,13 +14,28 @@ local Game = class('Game')
 function Game:initialize()
 end
 
-
 --------------------------------------------------------------------------------
 -- Get the default font.
 --
 -- @return love.graphics.newFont
 function Game.static:default_font()
    return love.graphics.newFont('assets/cozette_bitmap.ttf', 13)
+end
+
+--------------------------------------------------------------------------------
+-- Get the width of the window.
+--
+-- @return love.graphics.newFont
+function Game.static:width()
+   return love.graphics.getWidth()
+end
+
+--------------------------------------------------------------------------------
+-- Get the height of the window.
+--
+-- @return love.graphics.newFont
+function Game.static:height()
+   return love.graphics.getHeight()
 end
 
 return Game
