c5e4939ccbc8cc9542c998cc4da1ad908659854d
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-22 11:45:03 +0100

    Added ObjectFactory:generateSpriteRow() method.

--- FW/Factory/ObjectFactory.lua
+++ FW/Factory/ObjectFactory.lua
@@ -6,6 +6,7 @@ local class  = require 'libs/middleclass'
 local Color  = require 'FW.GFX.Color'
 local Object = require('FW.Core.Object')
 local Entity  = require('FW.Entity.Entity')
+local SpriteRow  = require('FW.GFX.SpriteRow')
 
 local ObjectFactory = class('ObjectFactory')
 
@@ -49,6 +50,25 @@ function ObjectFactory.static:generateEntities(count)
             local ry = love.math.random(10,600)
             local o  = Entity:new(rx,ry, 4)
             o.color  = Color:random()
+            _G.game.default_container:add(o)
+        end
+    end
+end
+
+--------------------------------------------------------------------------------
+-- Generate N number of SPRITERow objects for testing.
+--
+-- @tparam number count The number of objects to draw.
+--
+-- @return nil.
+function ObjectFactory.static:generateSpriteRows(count)
+    for x = 1,count do
+        for y = 1,count do
+            local rx = love.math.random(10,600)
+            local ry = love.math.random(10,600)
+            local o  = SpriteRow:new(rx,ry, 'assets/pixel-star.png',0,0,3)
+            o.current_frame = love.math.random(0,3)
+            o.color  = Color:randomBias(0.3)
             _G.game.default_container:add(o)
         end
     end
