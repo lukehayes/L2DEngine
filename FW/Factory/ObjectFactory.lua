93bbb0d456624174f065487aeeee8fb455be6d82
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-24 10:08:40 +0100

    SpriteRow tile_size is now set when calling ObjectFactory.generateSpriteRows()

--- FW/Factory/ObjectFactory.lua
+++ FW/Factory/ObjectFactory.lua
@@ -69,6 +69,7 @@ function ObjectFactory.static:generateSpriteRows(count)
             local ry = love.math.random(10, Game:height() * 2)
             local o  = SpriteRow:new(rx,ry, 'assets/pixel-star.png',0,0,5)
             o.current_frame = love.math.random(0,3)
+            o.tile_size = 5
             o.color  = Color:randomBias(0.3)
             _G.game.default_container:add(o)
         end
