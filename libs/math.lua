a2fbd95e305b108444b9f5dd7a7bc49ec1a9e3cb
Luke Hayes <lukepurplehayes@gmail.com>
2023-05-24 09:09:00 +0100

    All math functions are now available when required.

--- libs/math.lua
+++ libs/math.lua
@@ -26,11 +26,11 @@ function math.clamp(low, n, high) return math.min(math.max(low, n), high) end
 
 
 -- Linear interpolation between two numbers.
-function lerp(a,b,t) return (1-t)*a + t*b end
-function lerp2(a,b,t) return a+(b-a)*t end
+function math.lerp(a,b,t) return (1-t)*a + t*b end
+function math.lerp2(a,b,t) return a+(b-a)*t end
 
 -- Cosine interpolation between two numbers.
-function cerp(a,b,t) local f=(1-math.cos(t*math.pi))*.5 return a*(1-f)+b*f end
+function math.cerp(a,b,t) local f=(1-math.cos(t*math.pi))*.5 return a*(1-f)+b*f end
 
 
 -- Normalize two numbers.
@@ -54,14 +54,14 @@ function math.prandom(min, max) return love.math.random() * (max - min) + min en
 
 
 -- Checks if two line segments intersect. Line segments are given in form of ({x,y},{x,y}, {x,y},{x,y}).
-function checkIntersect(l1p1, l1p2, l2p1, l2p2)
+function math.checkIntersect(l1p1, l1p2, l2p1, l2p2)
     local function checkDir(pt1, pt2, pt3) return math.sign(((pt2.x-pt1.x)*(pt3.y-pt1.y)) - ((pt3.x-pt1.x)*(pt2.y-pt1.y))) end
     return (checkDir(l1p1,l1p2,l2p1) ~= checkDir(l1p1,l1p2,l2p2)) and (checkDir(l2p1,l2p2,l1p1) ~= checkDir(l2p1,l2p2,l1p2))
 end
 
 -- Checks if two lines intersect (or line segments if seg is true)
 -- Lines are given as four numbers (two coordinates)
-function findIntersect(l1p1x,l1p1y, l1p2x,l1p2y, l2p1x,l2p1y, l2p2x,l2p2y, seg1, seg2)
+function math.findIntersect(l1p1x,l1p1y, l1p2x,l1p2y, l2p1x,l2p1y, l2p2x,l2p2y, seg1, seg2)
     local a1,b1,a2,b2 = l1p2y-l1p1y, l1p1x-l1p2x, l2p2y-l2p1y, l2p1x-l2p2x
     local c1,c2 = a1*l1p1x+b1*l1p1y, a2*l2p1x+b2*l2p1y
     local det,x,y = a1*b2 - a2*b1
