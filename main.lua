e27764010676d6d9520506a0fc267f9c74db2aa0
lukehayes <lukepurplehayes@gmail.com>
2023-06-01 18:05:16 +0100

    Main includes QuadTree test.

--- main.lua
+++ main.lua
@@ -2,8 +2,19 @@ local Container     = require('FW.DS.Container')
 local EventManager  = require('FW.EventManager')
 local Game          = require('FW.Core.Game')
 
+local QuadTree      = require('FW.DS.QuadTree')
+local Entity        = require('FW.Entity.Entity')
+local Renderer      = require('FW.GFX.Renderer')
+local Rect          = require('FW.Shape.Rect')
+
 local em        = EventManager:new()
 em:emit('init')
+
+local qt = QuadTree:new(Rect:new(0,0,Game:width(), Game:height()))
+local nw = Rect:new(0,0, Game:width() / 2, Game:height() / 2)
+local ne = Rect:new(Game:width() / 2,0, Game:width() / 2, Game:height() / 2)
+local se = Rect:new(Game:width() / 2, Game:height() / 2, Game:width() / 2, Game:height() / 2)
+local sw = Rect:new(0, Game:height() / 2, Game:width() / 2, Game:height() / 2)
 
 
 function love.load()
@@ -34,6 +45,76 @@ function love.draw()
 
     _G.game.default_container:draw()
 
+    qt:drawRects()
+
+
     em:emit('draw_end')
+end
+
+
+function drawQtPoints(qt)
+
+    for _,e in ipairs(qt.entities) do
+        love.graphics.rectangle('line',
+            e.x,
+            e.y,
+            e.w,
+            e.h
+        )
+    end
+
+    
+    if(qt.northwest ~= nil) then
+        drawQtPoints(qt.northwest.entities)
+    end
+
+end
+
+function drawQuadTree(qt)
+
+    love.graphics.rectangle('line',
+        qt.northeast.bounds.x,
+        qt.northeast.bounds.y,
+        qt.northeast.bounds.w,
+        qt.northeast.bounds.h
+    )
+
+    love.graphics.rectangle('line',
+        qt.northwest.bounds.x,
+        qt.northwest.bounds.y,
+        qt.northwest.bounds.w,
+        qt.northwest.bounds.h
+    )
+
+    love.graphics.rectangle('line',
+        qt.southeast.bounds.x,
+        qt.southeast.bounds.y,
+        qt.southeast.bounds.w,
+        qt.southeast.bounds.h
+    )
+
+    love.graphics.rectangle('line',
+        qt.southwest.bounds.x,
+        qt.southwest.bounds.y,
+        qt.southwest.bounds.w,
+        qt.southwest.bounds.h
+    )
+
+    drawQuadTree(qt.northeast)
+    drawQuadTree(qt.northwest)
+    drawQuadTree(qt.southeast)
+    drawQuadTree(qt.southwest)
+end
+
+function love.keypressed(key)
+   if key == "space" then
+       local rx = love.math.random(0,Game:width() - 20)
+       local ry = love.math.random(0,Game:height() - 20)
+       local e = Entity:new(rx,ry, 10)
+
+
+       qt:insert(e)
+
+   end
 end
 
